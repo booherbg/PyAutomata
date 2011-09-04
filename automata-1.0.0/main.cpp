c6f6e409a3a7158d5945d3de9ab1afad7b72567f
Blaine Booher <bgbooher@gmail.com>
2011-09-04 00:45:56 -0400

    Modified main.cpp, mostly cosmetic

--- automata-1.0.0/main.cpp
+++ automata-1.0.0/main.cpp
@@ -11,13 +11,21 @@ int main()
 {
 	Automata automata = Automata(110);
 	cout << "p_rule: " << automata.p_rule << "\n";
+        /*
 	int i;
 	for (i=0;i<10;i++)
 	{
 		//automata.uintFromCurrentGeneration();
+                // uncomment the following if you don't want to use
+                // fillBuffer()
 		automata.iterateAutomata();
 	}
+        */
+
 	//unsigned int *c = (unsigned int *)malloc(sizeof(unsigned int)*chunks);
 	//cout << automata;
+        //automata.printBuffer();
+        automata.fillBuffer(); // just to be safe, fill up the buffer
+        automata.printBuffer();
 
 }
