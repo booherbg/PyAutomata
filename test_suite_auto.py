6c593258168811cd6dad79e9596b95353d682bf4
Blaine Booher <bgbooher@gmail.com>
2011-11-15 13:37:34 -0500

    added some more documentation and a test for 1M generations.
    iterestingly, pypy doesn't choke at 10M but cython does. either
    memory or cpu, not sure which.

--- test_suite_auto.py
+++ test_suite_auto.py
@@ -3,6 +3,15 @@ Same as test_suite.py but it only uses the module, not both python and c++
 
 this can be ran with pypy (which will load python) or cpython (which will use c++)
 
+Cpython with c++: the fastest. Chokes on high generations (10,000,000) although
+    technically that shouldn't happen.
+Cpython with python: the slowest, should never really use. (10x-20x slower)
+pypy with python: Only half as slow as cpython/c++, efficient memory that doesn't
+    choke on high generations. Increases performance as generations increase due
+    to the caching nature of pypy's JIT compiler.
+    recommended in most cases except when absolute performance is needed and
+    memory is not a concern.
+
 '''
 import __init__ as pa
 if pa.__pypy__:
@@ -31,11 +40,14 @@ for i in xrange(10):
 #assert a.chunks_FromCurrentGeneration(8) == b.chunks_FromCurrentGeneration(8)
 print '10 generations passes'
 
-for i in xrange(1000):
+print "stress test: 1,000,000 generations"
+for i in xrange(1000000):
+    if i % 50000 == 0:
+        print "%d generations done" % i
     a.iterateAutomata()
     #b.iterateAutomata()
 #assert a.chunks_FromCurrentGeneration(8) == b.chunks_FromCurrentGeneration(8)
-print '1000 generations passes'
+print '1000000 generations passes'
 
 a.printBuffer()
 print '---------------'
