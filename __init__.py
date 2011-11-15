56a2556995529ac6d9e03ebf7cac496ef1d3138e
Blaine Booher <bgbooher@gmail.com>
2011-11-15 13:21:21 -0500

    Added true pypy detection to the module.
    If you run with pypy, pure python will be loaded
    If you run with cpython, C++ will be loaded. Awesome!

--- __init__.py
+++ __init__.py
@@ -1,7 +1,13 @@
-python = False
+# Detecting pypy, thanks pypy-dev!
+try:
+    import __pypy__
+except ImportError:
+    __pypy__ = None
 
-if python:
+if __pypy__:
+    # Import pure python for pypy to munch on
     import automata as pya
 else:
+    # Import cython and C++ bindings for cpython to munch on
     import pyAutomata as pya
 pyautomata = pya.pyautomata
